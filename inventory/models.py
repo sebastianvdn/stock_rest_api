46c50206e42d0e0f6061814aeae6a1a4f0d3f08a
sebastianvdn <sebastian.van.den.noortgaete@gmail.com>
2020-06-06 16:33:07 +0200

    set serial_number to pk

--- inventory/models.py
+++ inventory/models.py
@@ -4,7 +4,8 @@ class Product(models.Model):
     serial_number = models.CharField(
         db_index=True,
         unique=True,
-        max_length=100
+        max_length=100,
+        primary_key=True
     )
     name = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
