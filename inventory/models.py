d0b51d6843799c0da3a5008718630d1ede57acc8
sebastianvdn <sebastian.van.den.noortgaete@gmail.com>
2020-06-06 15:57:51 +0200

    create api view

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    serial_number = models.CharField(
+        db_index=True,
+        unique=True,
+        max_length=100
+    )
+    name = models.CharField(max_length=100)
+    count = models.PositiveIntegerField()
+    price_p_p = models.FloatField(blank=True)
+
+    def __str__(self):
+        return self.name
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
