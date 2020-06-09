c3d960e7a23dda085543b2ef4d67047914e97653
sebastianvdn <sebastian.van.den.noortgaete@gmail.com>
2020-06-09 07:43:34 +0200

    modify display order

--- inventory/models.py
+++ inventory/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Product(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.PositiveIntegerField()
     barcode = models.CharField(
         db_index=True,
         unique=True,
         max_length=100,
         primary_key=True,
     )
-    name = models.CharField(max_length=100)
-    amount = models.PositiveIntegerField()
     price_piece = models.FloatField(blank=True, null=True)
 
     def __str__(self):
